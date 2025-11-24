0bfb419bc320ea50c7cbba30f98f7c2b652eaeaf
matthcol <matthias.colin@gmail.com>
2025-11-24 13:48:37 +0100

    First commit

---
+++ Bases/src/main/java/base/Application.java
@@ -0,0 +1,18 @@
+package base;
+
+public class Application {
+
+    // point d'entrée du programme
+    public static void main(String[] args) {
+        System.out.println("Bonjour, c'est lundi 😊");
+
+        // variable de type texte = String
+        String city = "Montauban";
+
+        // variable numérique entier: int
+        int nbPerson = 4;
+
+        System.out.println("nous sommes " + nbPerson + " à " + city); // concaténation
+    }
+
+}
---
+++ Bases/src/test/java/base/demo/DemoString.java
@@ -0,0 +1,23 @@
+package base.demo;
+
+import org.junit.jupiter.api.Test;
+
+public class DemoString {
+
+    @Test
+    void demo1(){
+        String city1 = "Montauban";
+        String city2 = "Toulouse";
+        String city3 = "L'Haÿ-les-Roses";
+        System.out.println(city1);
+        System.out.println(city1.toUpperCase()); // toUpperCase : méthode du type String
+        System.out.println(city3.toUpperCase());
+        System.out.println(city1.toLowerCase());
+        System.out.println(city3.substring(2, 5)); // Haÿ (débute à 0, endIndex exclus)
+    }
+
+    @Test
+    void demo2(){
+
+    }
+}
